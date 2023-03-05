711f7d7878eb40418e0c8488b9941d5a614c5ec5
anusha <anu.rapuru@gmail.com>
2023-03-04 21:20:32 -0800

    screens

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_example/reusable_widgets/resuable_widget.dart';
+import 'package:flutter_signin_example/screens/home_screen.dart';
+import 'package:flutter_signin_example/screens/signup_screen.dart';
+import 'package:flutter_signin_example/utils/color_utils.dart';
+import 'dart:developer';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  late String _error = "";
+  TextEditingController _passwordTextConrtoller = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();  
+  @override
+  Widget build(BuildContext context) {
+    log('data: hi');
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("FFFF8F"), 
+          hexStringToColor("FDDA0D"), 
+          hexStringToColor("FFD700")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+              20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                //logoWidget("assests/images/logoOnABudget.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter Email", Icons.person_outline, false, _emailTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextConrtoller),
+                SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, true, (){
+
+                  FirebaseAuth.instance.signInWithEmailAndPassword
+                    (email: _emailTextController.text,
+                      password: _passwordTextConrtoller.text).then((value){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                  }).onError((error, stackTrace){
+                    print("Error ${error.toString()}");
+                    setState(() {
+                      _error = error.toString();
+                    });
+
+                  });
+                  
+
+                }),
+
+                signUpOption(), //TODO: Not showing up for some reason, fix it
+                showAlert()
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget showAlert(){
+    print("Im here");
+    if(_error != ""){
+      return Container(
+        color: Colors.lime,
+        width: double.infinity,
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.error_outline),
+            ),
+            Expanded(child: AutoSizeText("No account found", maxLines: 3,),),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _error = "";
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return SizedBox(height: 0,);
+  }
+
+  Row signUpOption(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?", 
+            style:TextStyle(color: Colors.black87)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            " Sign Up",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+
+}
+
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,123 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_signin_example/reusable_widgets/resuable_widget.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+
+import '../utils/color_utils.dart';
+import 'home_screen.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  late String _error = "";
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _userNameTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Sign up",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("FFFF8F"), 
+          hexStringToColor("FDDA0D"), 
+          hexStringToColor("FFD700")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+              20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Username", Icons.person_outline, false, _userNameTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Email ID", Icons.person_outline, false, _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, false, () {
+                  FirebaseAuth.instance.createUserWithEmailAndPassword(email: _emailTextController.text,
+                      password: _passwordTextController.text).then((value) {
+                        print("CreatedNew Account");
+                        Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  }).onError((error, stackTrace) {
+                    print("Error ${error.toString()}");
+                    setState(() {
+                      _error = error.toString();
+                    });
+                  });
+                }),
+                showAlert()
+              ],
+
+            ),
+        ),),),
+    );
+
+  }
+  Widget showAlert(){
+    print("Im here");
+    if(_error != ""){
+      return Container(
+        color: Colors.lime,
+        width: double.infinity,
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.error_outline),
+            ),
+            Expanded(child: AutoSizeText("invalid input", maxLines: 3,),),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _error = "";
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return SizedBox(height: 0,);
+  }
+
+
+}
+
