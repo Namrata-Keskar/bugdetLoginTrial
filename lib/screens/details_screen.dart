b0eca1b6cebe3783ab835f1aea20edb7df3d0cd5
anusha <anu.rapuru@gmail.com>
2023-03-26 22:28:39 -0400

    Added the detail_screen that gives event details.

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //Code the button to go back and the firebaes button here
+    throw UnimplementedError();
+  }
+}
